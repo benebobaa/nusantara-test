b9ebef26cda9d53d5932d2466d37dfc55cc94d80
unknown <benediktus@students.amikom.ac.id>
2023-01-25 02:59:10 +0700

    remove unused

--- lib/data/service/constant/constant_api.dart
+++ lib/data/service/constant/constant_api.dart
@@ -1,15 +1,7 @@
 class ConstantApi {
-  ConstantApi._();
-
   /// Base Url
   static const String baseUrl =
       'https://basic-book-crud-e3u54evafq-et.a.run.app';
-
-  /// Receive Timeout
-  static const int receiveTimeout = 15000;
-
-  /// Conection Timeout
-  static const int connectionTimeout = 15000;
 
   /// Endpoints
   static const String loginEndpoint = '/api/login';
